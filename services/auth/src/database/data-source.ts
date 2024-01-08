2f287bbc768755d41e5c4bb30422cd2d2c5de06f
Bruno Koga <bruno.koga.fazano@gmail.com>
2024-01-08 14:53:15 -0300

    :sparkles: updates on auth service

--- services/auth/src/database/data-source.ts
+++ services/auth/src/database/data-source.ts
@@ -1,16 +1,19 @@
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
+import env from "../config/env"
 
 export const authDataSource = new DataSource({
-    type: "postgres",
-    host: "auth-database-srv",
-    port: 5432,
-    username: "admin",
-    password: "admin",
-    database: "auth_service_database",
-    entities: ["./entities/*.ts"],
+    type: env.database.type as any,
+    host: env.database.host,
+    port: env.database.port,
+    username: env.database.username,
+    password: env.database.password,
+    database: env.database.name,
+    entities: ['src/database/entities/*.entity.ts'],
+    migrations: ['src/database/migrations/*{.ts,.js}'],
+    migrationsTableName: 'migrations',
     logging: false,
     synchronize: false,
-})
+} as DataSourceOptions)
 
 export const init = async () => {
     authDataSource
