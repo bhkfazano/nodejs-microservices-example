6a601ea7626bde750067576b033d9950613fb9a1
Bruno Koga <bruno.koga.fazano@gmail.com>
2024-01-16 11:08:05 -0300

    :sparkles: added currentUser to global

--- common/interfaces/global.d.ts
+++ common/interfaces/global.d.ts
@@ -1,3 +1,13 @@
+import { UserDto } from "../dtos/user/user.dto";
+
 declare interface ErrorConstructor {
     captureStackTrace(targetObject: Object, constructorOpt?: Function): void;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserDto
+        }
+    }
 }
